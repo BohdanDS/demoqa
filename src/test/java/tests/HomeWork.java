b7259c14e9b6c2133d728aacca7bcff7f580b895
bohdan_peliutkevich <bohdanpeliutkevich@gmail.com>
2023-11-01 21:22:55 +0300

    FIll form home task

---
+++ src/test/java/tests/HomeWork.java
@@ -0,0 +1,48 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeWork {
+
+    @BeforeAll
+    static void beforeFillForm(){
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl= "https://demoqa.com/";
+        Configuration.holdBrowserOpen = false;
+
+    }
+
+    @Test
+    void fillForm(){
+        open("automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Bohdan");
+        $("#lastName").setValue("Peliutkevich");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userEmail").setValue("bohdan@gmail.com");
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__day--015").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#subjectsInput").setValue("Hindi").pressEnter().setValue("English").pressEnter();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/image1.png"));
+        $("#state").click();
+        $("#react-select-3-option-2").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        $("#currentAddress").setValue("Alabama");
+        $("#submit").click();
+    }
+}
