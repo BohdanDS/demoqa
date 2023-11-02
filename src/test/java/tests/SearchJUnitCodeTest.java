967724a43c846732fa734e41fe9c4c4070d8fc16
bohdan_peliutkevich <bohdanpeliutkevich@gmail.com>
2023-11-02 20:26:12 +0300

    Upd

--- src/test/java/tests/SearchJUnitCodeTest.java
+++ src/test/java/tests/SearchJUnitCodeTest.java
@@ -8,35 +8,15 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SelenideGitHubSearchTest {
+public class SearchJUnitCodeTest {
     @BeforeAll
     static void beforeAlltest(){
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl= "https://github.com/";
-
         Configuration.holdBrowserOpen = false;
     }
-    @Test
-    void searchSelenideOnGithub (){
-//        Bad locators to practice collection
-        open("selenide/selenide");
-        $("#wiki-tab").click();
-        $$("[data-filterable-for='wiki-pages-filter'] li").last().$("button").click();
-        $("[data-filterable-for='wiki-pages-filter']").shouldHave(text("SoftAssertions"));
-        $("[data-filterable-for='wiki-pages-filter']").$(byText("SoftAssertions")).click();
-        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-                "class Tests {\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }\n" +
-                "}"));
-    }
+
     @Test
     void searchJUnitCodeOnGithub(){
         open("selenide/selenide");
